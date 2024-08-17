7ba8d0becc9c4ea7fc4521f55c84fe393bc96e45
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-08-17 16:48:53 +0530

    Time: 15 ms (12.33%), Space: 7.5 MB (38.14%) - LeetHub

---
+++ 1033-broken-calculator/1033-broken-calculator.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        int result=0;
+        if(target<=startValue){
+            return abs(target-startValue);
+        }
+        while(target!=startValue){
+            if(target>startValue && target%2==0){
+                target=target/2;
+            }
+            else{
+                target++;
+            }
+            result++;
+        }
+        return result;
+    }
+};
