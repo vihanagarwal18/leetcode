38adba7ef416277d3d2fb1b34d0530b903b2d741
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-08-17 12:38:34 +0530

    Time: 13 ms (19.82%), Space: 14.7 MB (6.35%) - LeetHub

--- 0985-bag-of-tokens/0985-bag-of-tokens.cpp
+++ 0985-bag-of-tokens/0985-bag-of-tokens.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
         multiset<int> u(tokens.begin(), tokens.end());
+        //multi set allow us to have duplicate values 
+
+        //set and multi set store data in increasing order(smallest first)
         int result = 0;
         int temp = 0;
         while (!u.empty()) {
             auto smaller = u.begin();      
             // auto larger = u.rbegin();  
-            auto larger = prev(u.end());
-            
+            auto larger = prev(u.end()); // to access last element 
+            //erase function dont take reverse iterator as a argument
             if (*smaller <= power) {
                 power -= *smaller;
                 temp++;
