fa02a5928eb8803236e30008e61dd77044651aa2
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-08-17 13:18:34 +0530

    Time: 142 ms (5.11%), Space: 73 MB (9.96%) - LeetHub

--- 0917-boats-to-save-people/0917-boats-to-save-people.cpp
+++ 0917-boats-to-save-people/0917-boats-to-save-people.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        sort(people.begin(), people.end());
-        int i=0;
-        int j=people.size()-1;
+        multiset<int>u(people.begin(), people.end());
         int count=0;
-        while(i<=j){
-            if(people[i]+people[j]<=limit && i!=j){
-                i++;
+        while(!u.empty()){
+            auto s=u.begin();
+            auto l=prev(u.end());
+            if(*s+*l<=limit && s!=l){
+                u.erase(s);
             }
+            u.erase(l);
             count++;
-            j--;
         }
         return count;        
     }
