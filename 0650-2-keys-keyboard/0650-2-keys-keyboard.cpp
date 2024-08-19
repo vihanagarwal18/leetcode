c2b700a70bcef71535cdc3a4c83f4a32b9fae0e0
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-08-19 11:53:24 +0530

    Time: 19 ms (27.69%), Space: 157.3 MB (8.56%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int solve(int current, int copy, int& n, vector<vector<int>>& memo) {
+        if (current == n) return 0;
+        if (memo[current][copy] != -1) return memo[current][copy];
+        int copyy=INT_MAX;
+        if (current!=copy) {
+            int temp = solve(current, current, n, memo);
+            if (temp != INT_MAX) {
+                copyy = 1 + temp;
+            }
+        }
+        int paste = INT_MAX;
+        if(current+copy<= n) {
+            int temp = solve(current + copy, copy, n, memo);
+            if (temp != INT_MAX) {
+                paste = 1 + temp;
+            }
+        }
+        return memo[current][copy] = min(copyy, paste);
+    }
+
+    int minSteps(int n) {
+        if (n == 1) return 0;
+        vector<vector<int>> memo(n+1,vector<int>(n+1,-1));
+        return 1 + solve(1,1,n,memo);
+    }
+};
