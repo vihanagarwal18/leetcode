72666a40a719fa081ae789a24c5346270073dbc9
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-08-20 08:48:54 +0530

    Time: 0 ms (100%), Space: 9.6 MB (68.55%) - LeetHub

---
+++ 0909-stone-game/0909-stone-game.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        // priority_queue<int> q(piles.begin(),piles.end());
+        // int alice=0;
+        // int 
+        return true;
+    }
+};
