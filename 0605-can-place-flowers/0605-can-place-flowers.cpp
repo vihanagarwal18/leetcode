e7248026a2a38fb67807e3fc47f5d4d76c8d1149
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-08-20 22:11:20 +0530

    Time: 9 ms (77.85%), Space: 23.1 MB (6.44%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -3,7 +3,9 @@ public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
         for(int i=0;i<flowerbed.size();i++){
             if(flowerbed[i]==0) { 
-                if((i==0 || flowerbed[i-1]==0) && (i==flowerbed.size()-1 || flowerbed[i+1]==0)){
+                bool leftcheck=(i==0 || flowerbed[i-1]==0);
+                bool rightcheck=(i==flowerbed.size()-1 || flowerbed[i+1]==0);
+                if( leftcheck && rightcheck){
                     n--;
                     flowerbed[i]=1;
                 }
