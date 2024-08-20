0feb9851d50427c2c838bf2e4ad983b6704c6172
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-08-20 08:50:13 +0530

    Time: 0 ms (100%), Space: 9.6 MB (68.55%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        // priority_queue<int> q(piles.begin(),piles.end());
-        // int alice=0;
-        // int 
+        // as there are more than 2 piles and sum of piles if odd 
+        //so player who will pick first will always win
+        //thus true in every case(alice wins)
         return true;
     }
 };
