030991f725c50c4ade7570b535487e636b0d18fa
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-08-20 15:35:44 +0530

    Time: 5 ms (46.21%), Space: 8.8 MB (25.98%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -1,26 +1,11 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        string prefix="";
-        string result="";
-        int index=0;
         int n=str1.size();
         int m=str2.size();
-        for(int i=0;i<n;i++){
-            prefix+=str1[i];
-            int l=prefix.size();
-            if(l>m) break;
-            string temp1="";
-            string temp2="";
-            //m/prefix.size() * prefix if this equal to str2 then new result has been found
-            for(int j=0;j<m/l;j++){
-                temp1+=prefix;
-            }
-            for(int j=0;j<n/l;j++){
-                temp2+=prefix;
-            }
-            if(temp1==str2 && temp2==str1) result=prefix;
-        }
+        if(str1+str2!=str2+str1) return "";
+        int g=gcd(m,n); //length of prefix which will divide both
+        string result=str1.substr(0,g);
         return result;
     }
 };
