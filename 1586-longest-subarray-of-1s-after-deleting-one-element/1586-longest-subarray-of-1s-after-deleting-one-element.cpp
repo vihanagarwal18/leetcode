ad95edde63664543b4e93f92176bcefe74bfb64e
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-08-21 21:43:19 +0530

    Time: 51 ms (14.28%), Space: 59 MB (10.38%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -14,6 +14,6 @@ public:
             
             maxcount=max(maxcount,i-left+1);
         }
-        return maxcount-1;
+        return maxcount-1;  //minimum one deletion need to be made
     }
 };
