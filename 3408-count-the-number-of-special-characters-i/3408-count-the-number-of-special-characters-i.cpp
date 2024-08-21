956ae16d6e7bd22f2f6c7b349fa6f28ae7c6ed10
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-08-21 15:43:27 +0530

    Time: 3 ms (59.3%), Space: 10.3 MB (21.85%) - LeetHub

--- 3408-count-the-number-of-special-characters-i/3408-count-the-number-of-special-characters-i.cpp
+++ 3408-count-the-number-of-special-characters-i/3408-count-the-number-of-special-characters-i.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        set<char> u(word.begin(),word.end());
+        unordered_set<char> u(word.begin(),word.end());
         int result=0;
         for(auto& p:u){
             if(islower(p)){
