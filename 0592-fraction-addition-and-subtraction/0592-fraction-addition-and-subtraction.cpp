cb8b0dd740328bc6d554dbdbe4f5ed8c06e28ed5
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-08-23 11:30:23 +0530

    Time: 3 ms (50.39%), Space: 7.6 MB (70.54%) - LeetHub

---
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string fractionAddition(string expression) {
+        int n = expression.size();
+        int numerator = 0, denominator = 1; // 0/1 is starting
+        int i = 0;
+        while (i < n) {
+            int sign = 1;
+            if (expression[i] == '+' || expression[i] == '-') {
+                if (expression[i] == '-') {
+                    sign = -1;
+                }
+                i++;
+            }
+            int num = 0;
+            while (i<n && isdigit(expression[i])) {
+                num=num*10+(expression[i] - '0');
+                i++;
+            }
+            num=num*sign;
+            i++; // for /
+            int denom = 0;
+            while (i<n && isdigit(expression[i])) {
+                denom=denom*10+(expression[i] - '0');
+                i++;
+            }
+            numerator= numerator*denom + num*denominator; //lcm liya hain
+            //purane fraction main naya jodd diya
+            denominator =denominator*denom;
+            int g = gcd(abs(numerator), denominator); //khuch common hain toh cancel karna padega
+            numerator=numerator/g;
+            denominator=denominator/g;
+        }
+        return to_string(numerator) + "/" + to_string(denominator);
+    }
+    int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+};
