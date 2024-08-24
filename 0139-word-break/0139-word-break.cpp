233bdffe6f04fd50e70e0c6ebbfcb2e7edd99797
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-08-24 18:35:38 +0530

    Time: 19 ms (26.99%), Space: 18.6 MB (22.1%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -7,8 +7,8 @@ public:
         for(int i=index;i<n;i++){
             string newstring=s.substr(index,i-index+1);
             if(wordset.find(newstring)!=wordset.end() && solve(i+1,n,memo,s,wordset)){
-                return memo[index]=true;
-                
+                cout<<newstring<<"::";
+                return memo[index]=true; 
             }
         }
         return memo[index]=false;
@@ -18,18 +18,5 @@ public:
         vector<int> memo(n,-1);
         unordered_set<string> wordset(wordDict.begin(),wordDict.end());
         return solve(0,n,memo,s,wordset);
-        // memo[0]=1;
-        // for(int i=1;i<=n;i++){
-        //     for(int j=0;j<=i;j++){
-        //         if(memo[j]==1){
-        //             string temp=s.substr(j,i-j+1);
-        //             if(wordset.find(temp)!=wordset.end()){
-        //                 //word exist
-        //                 memo[i]=1;
-        //             }
-        //         }
-        //     }
-        // }
-        // return memo[n];
     }
 };
