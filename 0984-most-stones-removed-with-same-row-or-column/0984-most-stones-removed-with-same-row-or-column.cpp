97496e928b7fb9bde82ea7341c55297809279c08
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-08-30 23:43:49 +0530

    Time: 183 ms (6.38%), Space: 17.9 MB (92.07%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
@@ -1,24 +1,30 @@
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
-    void dfs(set<vector<int>>& u, vector<int> stone,set<vector<int>>& visited,vector<vector<int>>& stones) {
-        for(auto& p:stones){
-            if(visited.find(p)==visited.end() && (p[0]==stone[0] ||p[1]==stone[1])){
-                u.erase(stone);
-                visited.insert(stone);
-                dfs(u,p,visited,stones);
+    void dfs(vector<vector<int>>& stones, vector<int>& stone, vector<bool>& visited, int index) {
+        visited[index] = true;
+        for (int i = 0; i < stones.size(); i++) {
+            if (!visited[i] && (stones[i][0] == stone[0] || stones[i][1] == stone[1])) {
+                dfs(stones, stones[i], visited, i);
             }
         }
     }
+
     int removeStones(vector<vector<int>>& stones) {
         int n = stones.size();
-        set<vector<int>> u(stones.begin(), stones.end());
-        set<vector<int>> visited;
+        vector<bool> visited(n, false);
         int groups = 0;
-        while (!u.empty()) {
-            auto stone = *u.begin();
-            dfs(u, stone,visited,stones);
-            groups++;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(stones, stones[i], visited, i);
+                groups++;
+            }
         }
+
         return n - groups;
     }
 };
