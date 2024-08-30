ddb0e0067c03dd74f43c241c7e9454afce671c1c
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-08-30 23:58:12 +0530

    Time: 183 ms (6.38%), Space: 18 MB (85.97%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     void dfs(vector<vector<int>>& stones, vector<int>& stone, vector<bool>& visited, int index) {
         visited[index] = true;
+        //cout<<"Stone removed is "<<stones[index][0]<<" "<<stones[index][1]<<endl;
         for (int i = 0; i < stones.size(); i++) {
             if (!visited[i] && (stones[i][0] == stone[0] || stones[i][1] == stone[1])) {
                 dfs(stones, stones[i], visited, i);
@@ -15,6 +16,7 @@ public:
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 dfs(stones, stones[i], visited, i);
+                //cout<<"new group started"<<endl;
                 groups++;
             }
         }
