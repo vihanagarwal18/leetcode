17eb93fadf71956f314e13b26ba48ac144967fb1
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-08-31 11:50:32 +0530

    Time: 234 ms (82.46%), Space: 106.1 MB (80.45%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,29 +1,24 @@
-#include <vector>
-#include <utility>
-
-using namespace std;
-
 class Solution {
 public:
-    void dfs(int& result, vector<vector<pair<int, int>>>& adj, int node, vector<int>& visited) {
-        visited[node] = 1;
-        for (auto& p : adj[node]) {
-            if (!visited[p.first]) {
-                if (p.second == -1) result++; // Count the reverse edge that needs to be reordered
-                dfs(result, adj, p.first, visited);
+    void dfs(int& result,vector<vector<pair<int,int>>>& adj,int node,vector<int>& visited){
+        visited[node]=1;
+        for(auto& p:adj[node]){
+            if(!visited[p.first]){
+                if(p.second==-1) result++;
+                dfs(result,adj,p.first,visited);
             }
         }
     }
-
     int minReorder(int n, vector<vector<int>>& connections) {
-        vector<vector<pair<int, int>>> adj(n);
-        for (auto& p : connections) {
-            adj[p[0]].push_back({p[1], -1}); // -1 means reverse edge (needs reorder)
-            adj[p[1]].push_back({p[0], 1});  // 1 means correct edge
+        vector<vector<pair<int,int>>> adj(n);//node direction
+        for(auto& p:connections){
+            adj[p[0]].push_back({p[1],-1});//-1 means revere edge 
+            adj[p[1]].push_back({p[0],1}); 
         }
-        int result = 0;
-        vector<int> visited(n, 0);
-        dfs(result, adj, 0, visited);
+        int result=0;
+        vector<int> visited(n,0);
+        // visited[0]=1;
+        dfs(result,adj,0,visited);
         return result;
     }
 };
