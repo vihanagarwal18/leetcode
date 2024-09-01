b75614a2d5fed0e737022c67c1487afd94c1594b
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-09-01 14:29:57 +0530

    Time: 178 ms (100%), Space: 146.9 MB (66.67%) - LeetHub

---
+++ 3531-minimum-amount-of-damage-dealt-to-bob/3531-minimum-amount-of-damage-dealt-to-bob.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool static comp(const pair<double, int>& a, const pair<double, int>& b) {
+        return a.first > b.first;
+    }
+
+    long long minDamage(int power, vector<int>& damage, vector<int>& health) {
+        int n = damage.size();
+        vector<int> time_to_kill(n);
+        for (int i = 0; i < n; i++) {
+            time_to_kill[i] = (health[i] + power - 1) / power; // Equivalent to ceil(health[i] / power)
+        }
+
+        vector<pair<double, int>> ratio(n);
+        for (int i = 0; i < n; i++) {
+            ratio[i] = {(double)(damage[i]) / time_to_kill[i], i};
+        }
+
+        sort(ratio.begin(), ratio.end(), comp); // Sorted according to the ratio, higher ratio first
+
+        long long total_damage = 0;
+        long long current_damage = 0;
+        for (int& dmg : damage) {
+            current_damage += dmg;
+        }
+
+        for (auto& p : ratio) {
+            int index = p.second;
+            total_damage += current_damage * time_to_kill[index];
+            current_damage -= damage[index];
+        }
+
+        return total_damage;
+    }
+};
