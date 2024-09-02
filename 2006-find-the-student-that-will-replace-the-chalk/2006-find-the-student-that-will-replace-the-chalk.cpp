71cd53079596735fa751dfa3f289e1368145c398
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-09-02 09:49:47 +0530

    Time: 98 ms (48.02%), Space: 77.1 MB (18.08%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
         int index=0;
-        int n=chalk.size();
         long long total=0;
         for(auto& p:chalk){
             total+=p;
         }
         k=k%total;
         while(k>=0){
-            int require=chalk[index%n];
-            if(k<require){
-                return index%n;
-            }
+            int require=chalk[index];
+            if(k<require) return index;
             index++;
             k-=require;
         }
