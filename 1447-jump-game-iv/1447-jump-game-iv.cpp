a97608a1648358fe7963c4846c0c1fda73dc2d72
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-09-07 19:50:07 +0530

    Time: 152 ms (65.48%), Space: 82 MB (56.53%) - LeetHub

--- 1447-jump-game-iv/1447-jump-game-iv.cpp
+++ 1447-jump-game-iv/1447-jump-game-iv.cpp
@@ -21,7 +21,7 @@ public:
                 vector<int>& nextIndices = m[arr[index]];
                 nextIndices.push_back(index-1);
                 nextIndices.push_back(index+1);
-                for (int next : nextIndices) {
+                for (int& next : nextIndices) {
                     if (next >= 0 && next < n && !visited[next]) {
                         visited[next] = true;
                         q.push(next);
@@ -36,44 +36,41 @@ public:
     }
 };
 
-/*
-class Solution {
-public:
-    int solve(int index, vector<int>& memo, vector<int>& arr, unordered_map<int, vector<int>>& m) {
-        if (index >= arr.size() - 1) return 0; 
-        if (memo[index] != -1) return memo[index]; 
-        int minimum = INT_MAX;
-        // +1 jump
-        if (index + 1 < arr.size()) {
-            int temp=1+solve(index + 1, memo, arr, m);
-            minimum=min(minimum, temp);
-        }
-        // -1 jump
-        if (index > 0) {
-            int temp=1+solve(index-1,memo,arr,m);
-            minimum=min(minimum,temp);
-        }
-        for (int next : m[arr[index]]) {
-            if (next != index) {
-                int temp = 1 + solve(next, memo, arr, m);
-                minimum = min(minimum, temp);
-            }
-        }
-        m[arr[index]].clear(); //to reduce no. of recurssive call
-        memo[index] = minimum; 
-        return memo[index];
-    }
 
-    int minJumps(vector<int>& arr) {
-        int n = arr.size();
-        unordered_map<int, vector<int>> m;
-        for (int i = 0; i < n; i++) {
-            m[arr[i]].push_back(i);
-        }
-        vector<int> memo(n, -1); 
-        return solve(0, memo, arr, m);
-    }
-};
+// class Solution {
+// public:
+//     int solve(int index, vector<int>& memo, vector<int>& arr, unordered_map<int, vector<int>>& m) {
+//         if (index >= arr.size() - 1) return 0; 
+//         if (memo[index] != -1) return memo[index]; 
+//         int minimum = INT_MAX;
+//         // +1 jump
+//         if (index + 1 < arr.size()) {
+//             int temp=1+solve(index + 1, memo, arr, m);
+//             minimum=min(minimum, temp);
+//         }
+//         // -1 jump
+//         if (index > 0) {
+//             int temp=1+solve(index-1,memo,arr,m);
+//             minimum=min(minimum,temp);
+//         }
+//         for (int& next : m[arr[index]]) {
+//             if (next != index) {
+//                 int temp = 1 + solve(next, memo, arr, m);
+//                 minimum = min(minimum, temp);
+//             }
+//         }
+//         m[arr[index]].clear(); //to reduce no. of recurssive call
+//         memo[index] = minimum; 
+//         return memo[index];
+//     }
 
-
-*/
+//     int minJumps(vector<int>& arr) {
+//         int n = arr.size();
+//         unordered_map<int, vector<int>> m;
+//         for (int i = 0; i < n; i++) {
+//             m[arr[i]].push_back(i);
+//         }
+//         vector<int> memo(n, -1); 
+//         return solve(0, memo, arr, m);
+//     }
+// };
