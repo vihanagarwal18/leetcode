61f3cc5d32f462dc9703bdd7085c6abdfa9db4e6
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-09-07 19:39:20 +0530

    Time: 35 ms (53.35%), Space: 38.7 MB (45.45%) - LeetHub

--- 1428-jump-game-iii/1428-jump-game-iii.cpp
+++ 1428-jump-game-iii/1428-jump-game-iii.cpp
@@ -3,17 +3,17 @@ public:
     bool solve(int index,vector<int>& memo,vector<int>& arr){
         if(arr[index]==0) return true;
         if(memo[index]!=-1) return memo[index];
+        // int n=arr.size();
         bool forward=false;
         bool backward=false;
         memo[index]=0;
         if(index+arr[index]<arr.size()){
-            //can jump forward
             forward=solve(index+arr[index],memo,arr);
         }
         if(index-arr[index]>=0){
             backward=solve(index-arr[index],memo,arr);
         }
-        memo[index]=forward || backward;
+        memo[index]=backward||forward;
         return memo[index];
     }
     bool canReach(vector<int>& arr, int start) {
