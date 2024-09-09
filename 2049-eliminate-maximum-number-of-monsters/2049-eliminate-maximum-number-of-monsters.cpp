09d9bdd26c129803c8ce44e1a552b3a4ddffd0a9
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-09-10 00:19:37 +0530

    Time: 115 ms (17.17%), Space: 92.9 MB (9.67%) - LeetHub

--- 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.cpp
+++ 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
-        int n=dist.size();
-        priority_queue<pair<double,int> ,vector<pair<double,int>>,greater<>> pq;
-        for(int i=0;i<n;i++){
-            pq.push({(double)dist[i]/speed[i],i}); //time ,index
+        int n = dist.size();
+        priority_queue<double, vector<double>, greater<>> pq;
+        
+        for (int i = 0; i < n; i++) {
+            pq.push((double)dist[i] / speed[i]); // only store time
         }
-        int time=1;
-        int result=1;
-        pq.pop(); //first monster would be killed in startign only
-        while(pq.size()>0){
-            auto p=pq.top();
-            if(p.first>time){
+
+        int time = 1;
+        int result = 1;
+        pq.pop(); // first monster would be killed at the start
+        
+        while (!pq.empty()) {
+            double monsterTime = pq.top();
+            if (monsterTime > time) {
                 time++;
                 pq.pop();
                 result++;
+            } else {
+                return result;
             }
-            else return result;
         }
         return result;
     }
