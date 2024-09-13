cfa85fdb77620d8f433218d23d7305949ec1ccba
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-09-14 01:54:36 +0530

    Time: 9 ms (90.39%), Space: 20.1 MB (74.85%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -5,21 +5,20 @@ public:
         vector<int> candy(n, 1);  
 
         // phele left neighbour ke sangh
-        for (int i = 1; i < n; ++i) {
-            if (rating[i] > rating[i - 1]) {
-                candy[i] = candy[i - 1] + 1;
+        for (int i=1;i<n;i++) {
+            if (rating[i]>rating[i-1]){
+                candy[i]=candy[i-1]+1;
             }
         }
-
         // ab right neighbour ke sangh
-        for (int i = n - 2; i >= 0; --i) {
-            if (rating[i] > rating[i + 1]) {
-                candy[i] = max(candy[i], candy[i + 1] + 1);
+        for (int i=n-2;i>=0;i--) {
+            if (rating[i]>rating[i+1]) {
+                candy[i]=max(candy[i],candy[i+1]+1);
             }
         }
-        int result = 0;
+        int result=0;
         for (auto& c : candy) {
-            result += c;
+            result+=c;
         }
         return result;
     }
