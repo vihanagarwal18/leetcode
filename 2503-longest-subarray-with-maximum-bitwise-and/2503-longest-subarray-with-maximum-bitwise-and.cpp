63c3ce32bca34de4ddde43ffe3c7bccca5858a0f
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-09-14 09:40:16 +0530

    Time: 121 ms (8.33%), Space: 84.9 MB (34.21%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
@@ -3,16 +3,19 @@ public:
     int longestSubarray(vector<int>& nums) {
         int n=nums.size();
         int result=0;
-        //maximum bitwise of this array would be max value
-        int maxvalue=*max_element(nums.begin(),nums.end());
-        //now need to find longest subaarray who only has 3
-        int i=0;
-        for(int j=0;j<n;j++){
-            if(nums[j]!=maxvalue){
-                i=j+1;
+        int maxx=-1;
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]>maxx){
+                result=1;
+                j=i;
+                maxx=nums[i];
+            }
+            else if(nums[i]==maxx){
+                result=max(result,i-j+1);
             }
             else{
-                result=max(result,j-i+1);
+                j=i+1;
             }
         }
         return result;
