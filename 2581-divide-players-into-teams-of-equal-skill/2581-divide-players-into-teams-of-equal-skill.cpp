7c5945eaf101bdc73f418c15158d0388391b0276
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-10-04 19:40:40 +0530

    Time: 59 ms (80.2%), Space: 57.2 MB (16.2%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
@@ -5,14 +5,12 @@ public:
         int n = skill.size();
         if (n%2 == 1) return -1;
         if (n == 2) return (long long)skill[0] * skill[1];
-        
         long long total = 0;
-        map<int, int> m;
+        unordered_map<int, int> m;
         for (auto& p : skill) {
             total += p;
             m[p]++;
         }
-        
         if (total%(n/2) != 0) return -1;
         long long sum = total / (n/2);
 
