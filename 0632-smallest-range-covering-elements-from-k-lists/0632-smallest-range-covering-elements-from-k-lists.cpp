2d2d5e80311d23a9e57ace3465dc46ec9cc69936
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-10-13 14:40:08 +0530

    Time: 69 ms (18.53%), Space: 24 MB (18.53%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -14,26 +14,24 @@ public:
         vector<int> index(n,0); 
         for(int i=0;i<n;i++){
             minheap.push({nums[i][0],i,1});
-            if(nums[i][0]>maximum) maximum=nums[i][0];
+            maximum = max(maximum, nums[i][0]);
         }
-        bool flag=true;
         vector<int> result={minheap.top()[0],maximum};
-        while(flag){
+        while(true){
             int minimum=minheap.top()[0];
             int index=minheap.top()[1];
             int arrindex=minheap.top()[2];
             minheap.pop();
-            int temp=maximum-minimum;
-            if(temp<result[1]-result[0]){
+            
+            if(maximum-minimum<result[1]-result[0]){
                 result[0]=minimum;
                 result[1]=maximum;
             }
-            if(arrindex==nums[index].size()){
-                flag=false;
+            if(arrindex>=nums[index].size()){
                 break;
             }
             minheap.push({nums[index][arrindex],index,arrindex+1});
-            if(nums[index][arrindex]>maximum) maximum=nums[index][arrindex];
+            maximum=max(maximum,nums[index][arrindex]);
         }
         return result;
     }
