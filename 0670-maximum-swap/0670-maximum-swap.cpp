68d535963a730cb6e5dc6624991ec9408d8becd5
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-10-17 12:08:55 +0530

    Time: 0 ms (100%), Space: 7.8 MB (12.29%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string number="";
+        int temp=num;
+        while(temp>0){
+            number=to_string(temp%10)+number;
+            temp=temp/10;
+        }
+        int n=number.size();
+        int result=num;
+        for(int i=0;i<n-1;i++){
+            int right=i+1;
+            for(int j=right;j<n;j++){
+                if(number[j]>number[i]){
+                    string tem=number;
+                    swap(tem[i],tem[j]);
+                    result=max(result,stoi(tem));
+                }
+            }
+        }
+        return result;
+    }
+};
