5705905383ab43512cdbd68f84cd06a94cc09ade
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-10-17 12:30:13 +0530

    Time: 3 ms (38.18%), Space: 7.8 MB (12.29%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -13,9 +13,10 @@ public:
             int right=i+1;
             for(int j=right;j<n;j++){
                 if(number[j]>number[i]){
-                    string tem=number;
-                    swap(tem[i],tem[j]);
-                    result=max(result,stoi(tem));
+                    // string tem=number;
+                    swap(number[i],number[j]);
+                    result=max(result,stoi(number));
+                    swap(number[i],number[j]);
                 }
             }
         }
