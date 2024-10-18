38df448fc47a44451fe93cc693b4799c9053eeab
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-10-18 18:34:21 +0530

    Time: 3 ms (99.75%), Space: 10.2 MB (48.34%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
+    //this code has time complexity of O(2^N)
     int solve(vector<int>& nums,int index,int value,int& maxx,int n){
         if(value==maxx){
             return pow(2,n-index);
+            //eske andar we took the subset till index ,then since this subset already makes maxx
+            //we will have pow(2,n-index) combinations of picking other elements
         }
         if(index==n) return 0;
         int take=solve(nums,index+1,value|nums[index],maxx,n);
@@ -15,6 +18,4 @@ public:
         for(auto& p:nums) maxx|=p; //this is the max possible or
         return solve(nums,0,0,maxx,n);
     }
-
-    //this code has time complexity of O(2^N)
 };
