bdd541184f96a4b28d449afbe1bc4123288b2779
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-10-23 10:37:37 +0530

    Time: 174 ms (91.76%), Space: 369.5 MB (9.8%) - LeetHub

---
+++ 2677-cousins-in-binary-tree-ii/2677-cousins-in-binary-tree-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    TreeNode* replaceValueInTree(TreeNode* root) {
+       queue<pair<TreeNode*,TreeNode*>> q; //child parent
+       q.push({root,NULL});
+       while(!q.empty()){
+        int n=q.size();
+        map<TreeNode*,int> mp; //parent aur uske bacho ka sum
+        int totalsum=0;
+        vector<pair<TreeNode*,TreeNode*>> cousins;
+        for(int i=0;i<n;i++){
+            auto cur=q.front(); 
+            q.pop();
+            totalsum+=cur.first->val;
+            mp[cur.second]+=cur.first->val;
+            cousins.push_back(cur);
+            if(cur.first->left) q.push({cur.first->left,cur.first});
+            if(cur.first->right) q.push({cur.first->right,cur.first});
+        }
+        for(auto& it:cousins) it.first->val=totalsum-mp[it.second];
+       }
+       return root; 
+    }
+};
