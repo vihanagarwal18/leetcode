0728fbbfa86dfc61aa475a6641928d1f8d6d5a48
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-10-23 11:30:27 +0530

    Time: 0 ms (100%), Space: 10 MB (45.04%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    void solve(vector<vector<int>>& result,int count,int& n,vector<int>& nums,vector<int> temp,unordered_set<int>& u){
-        if(count==n){
+    void solve(vector<vector<int>>& result, int count, int& n, vector<int>& nums, vector<int>& temp, vector<bool>& visited) {
+        if (count == n) {
             result.push_back(temp);
             return;
         }
-        for(int i=0;i<n;i++){
-            if(u.find(nums[i])==u.end()){
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
                 temp.push_back(nums[i]);
-                u.insert(nums[i]);
-                solve(result,count+1,n,nums,temp,u);
+                visited[i] = true;
+                solve(result, count + 1, n, nums, temp, visited);
                 temp.pop_back();
-                u.erase(nums[i]);
+                visited[i] = false;
             }
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> result;
-        int n=nums.size();
-        unordered_set<int> u;
+        int n = nums.size();
+        vector<bool> visited(n, false);
         vector<int> temp;
-        solve(result,0,n,nums,temp,u);
+        solve(result, 0, n, nums, temp, visited);
         return result;
     }
 };
