b5afe5d0caf36e75dd95a97e995314e99f551e62
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-10-28 23:16:46 +0530

    Time: 186 ms (8.89%), Space: 131.2 MB (13.83%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
@@ -4,14 +4,11 @@ public:
         if (memo.find(nums[i]) != memo.end()) {
             return memo[nums[i]];
         }
-
         int squareRoot = sqrt(nums[i]);
         int maxStreak = 1;
-
         if (squareRoot * squareRoot == nums[i] && m.count(squareRoot)) {
             maxStreak = 1 + solve(m[squareRoot], nums, memo, m);
         }
-
         memo[nums[i]] = maxStreak;
         return maxStreak;
     }
@@ -19,20 +16,15 @@ public:
     int longestSquareStreak(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-
         unordered_map<int, int> memo;
         unordered_map<int, int> m;
-
         for (int i = 0; i < n; i++) {
             m[nums[i]] = i;
         }
-
         int maxStreak = 1;
-
         for (int i = 0; i < n; i++) {
             maxStreak = max(maxStreak, solve(i, nums, memo, m));
         }
-
         return maxStreak > 1 ? maxStreak : -1;
     }
 };
