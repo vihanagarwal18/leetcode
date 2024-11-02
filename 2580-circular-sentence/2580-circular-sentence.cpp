7e32a274f839705c79c3175c1be5d7fa1dfd14e8
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-11-02 15:07:40 +0530

    Time: 0 ms (100%), Space: 8.2 MB (59.78%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.cpp
+++ 2580-circular-sentence/2580-circular-sentence.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     bool isCircularSentence(string sentence) {
         int n=sentence.size();
-        for(int i=0;i<n;i++){
-            if(sentence[i]==' '){
-                if(sentence[i+1]!=sentence[i-1]) return false;
-            }
-        }
         if(sentence[n-1]!=sentence[0]) return false;
+        for(int i=0;i<n;i++){
+            if(sentence[i]==' ' && sentence[i+1]!=sentence[i-1]) return false;
+        }
         return true;
     }
 };
