f0ab7ba77d41937a60fb117f218b9ff91bc712a2
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-11-07 12:59:14 +0530

    Time: 36 ms (37.31%), Space: 60.1 MB (74.4%) - LeetHub

---
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int largestCombination(vector<int>& candidates) {
+        int n=candidates.size();
+        vector<int> bitscount(25,0);
+        for(auto& p:candidates){
+            int index=0;
+            while(p>0){
+                if(p&1==1)  bitscount[index]++;
+                p=p/2;
+                index++;
+            }
+        }
+        int maxx=0;
+        for(auto& p:bitscount){
+            if(p>maxx) maxx=p;
+        }
+        return maxx;
+
+    }
+};
