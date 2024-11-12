52aef330282eabad432da97a0bc2db02116abe5f
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-11-13 00:05:09 +0530

    Time: 15 ms (36.69%), Space: 33.9 MB (48.45%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int countdays(vector<int>& weights, int capacity) {
-        int result = 1; 
-        int w= 0;
+        int result=1; 
+        int w=0;
         for (int& p: weights) {
             if(p>capacity) return INT_MAX;
             if (w+p > capacity) {
@@ -21,7 +21,6 @@ public:
         while(left<=right){
             int mid=left+(right-left)/2;
             int d=countdays(weights,mid);
-            cout<<mid<<" "<<d<<endl;
             if(d>days){
                 left=mid+1;
             }
