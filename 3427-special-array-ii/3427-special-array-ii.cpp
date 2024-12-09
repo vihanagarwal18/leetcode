6960a3879a9c4c15e65b497115ddffdeae79c73a
VIHAN AGARWAL <98251592+vihanagarwal18@users.noreply.github.com>
2024-12-09 11:11:34 +0530

    Time: 1 ms (94.14%), Space: 128 MB (50.48%) - LeetHub

---
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        int n=nums.size();
+        vector<int> prefixcountpair;
+        prefixcountpair.push_back(0); //dummy
+        int count=0;
+        for(int i=1;i<n;i++){
+            if((nums[i-1]%2==0 && nums[i]%2==1) || (nums[i]%2==0 && nums[i-1]%2==1)){
+                count++;
+            }
+            prefixcountpair.push_back(count);
+        }
+        vector<bool> result;
+        for(auto& p:queries){
+            if(p[0]==p[1]) result.push_back(true);
+            else if(prefixcountpair[p[1]]-prefixcountpair[p[0]]==p[1]-p[0]) result.push_back(true);
+            else result.push_back(false);
+        }
+        return result;
+    }
+};
